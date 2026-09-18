d16baee804a7ac31a5a7b5bc96a3c98cc8d87fde
srdjankarlo <srkidjan@gmail.com>
2026-09-18 02:55:28 +0200

    FEAT(): Ball moving.

--- src/main.ts
+++ src/main.ts
@@ -7,39 +7,100 @@ import Phaser from "phaser";
  * For now, BattleBall'z has only one scene: the arena.
  */
 class BattleBallzScene extends Phaser.Scene {
-    constructor() {
-        super("BattleBallzScene");
+  private ball!: Phaser.GameObjects.Arc;
+
+  // Ball movement.
+  private velocityX = 150;
+  private velocityY = 100;
+
+  // Arena dimensions.
+  private readonly arenaWidth = 500;
+  private readonly arenaHeight = 500;
+
+  // Ball radius.
+  private readonly ballRadius = 20;
+
+  constructor() {
+    super("BattleBallzScene");
+  }
+
+  create() {
+    console.log("BattleBallz scene created");
+
+    // Draw a simple 500x500 arena.
+    this.add.rectangle(
+      this.arenaWidth / 2,
+      this.arenaHeight / 2,
+      this.arenaWidth,
+      this.arenaHeight,
+      0x333333
+    );
+
+    // Arena border.
+      this.add.rectangle(
+        250,
+        250,
+        500,
+        500
+      )
+      .setStrokeStyle(20, 0x4d0feb);
+
+    // Add the first BattleBall'z unit.
+    this.ball = this.add.circle(
+      this.arenaWidth / 2,
+      this.arenaHeight / 2,
+      this.ballRadius,
+      0xff0000
+    );
+
+    console.log("Ball created:", this.ball);
+  }
+
+  update(_time: number, delta: number) {
+    // delta is the amount of time since the previous frame,
+    // measured in milliseconds.
+    //
+    // Convert it to seconds because our velocity is expressed
+    // in pixels per second.
+    const deltaSeconds = delta / 1000;
+
+    // Move the ball.
+    this.ball.x += this.velocityX * deltaSeconds;
+    this.ball.y += this.velocityY * deltaSeconds;
+
+    // Left/right wall.
+    if (this.ball.x - this.ballRadius <= 0) {
+      this.ball.x = this.ballRadius;
+      this.velocityX *= -1;
     }
 
-    create() {
-        // Draw a simple 500x500 arena.
-        this.add.rectangle(
-            250,
-            250,
-            500,
-            500,
-            0x222222
-        );
-
-        // Add the first BattleBall'z unit.
-        this.add.circle(
-            250,
-            250,
-            20,
-            0x3498db
-        );
+    if (this.ball.x + this.ballRadius >= this.arenaWidth) {
+      this.ball.x = this.arenaWidth - this.ballRadius;
+      this.velocityX *= -1;
     }
+
+    // Top/bottom wall.
+    if (this.ball.y - this.ballRadius <= 0) {
+      this.ball.y = this.ballRadius;
+      this.velocityY *= -1;
+    }
+
+    if (this.ball.y + this.ballRadius >= this.arenaHeight) {
+      this.ball.y = this.arenaHeight - this.ballRadius;
+      this.velocityY *= -1;
+    }
+  }
 }
 
 const config: Phaser.Types.Core.GameConfig = {
-    type: Phaser.AUTO,
+  type: Phaser.AUTO,
 
-    width: 500,
-    height: 500,
+  width: 500,
+  height: 500,
 
-    backgroundColor: "#111111",
+  backgroundColor: "#000000",
 
-    scene: BattleBallzScene,
+  scene: BattleBallzScene,
 };
 
 new Phaser.Game(config);
